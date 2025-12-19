d91009c5126f6cd4eb6fe55cb2e44c627fcfddcb
Ian Donegan <IanDonegan@IanDonegan.com>
2025-12-19 12:34:18 -0500

    Moved counting function to stats.py.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from stats import get_num_words
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(f"Found {len(file_contents.split())} total words")
+    print(f"Found {get_num_words(file_contents)} total words")
 
 main() 

7d292fa9d47f76efe6559a259a9e32ec099a02c6
Ian Donegan <IanDonegan@IanDonegan.com>
2025-12-19 14:29:51 -0500

    Now accepting arguments.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import sys
 from stats import get_num_words, get_character_dict, character_dict_to_list, print_alpha_report
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    target = sys.argv[1]
+    with open(target) as f:
         file_contents = f.read()
     print(f"Found {get_num_words(file_contents)} total words")
     character_dict = get_character_dict(file_contents)

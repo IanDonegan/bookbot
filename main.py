2a0a03c97430d28ce5fa812fd2cdd510fd2a3d96
Ian Donegan <IanDonegan@IanDonegan.com>
2025-12-19 12:47:28 -0500

    Count all words, then count each character.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_num_words
+from stats import get_num_words, get_character_counts
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(f"Found {get_num_words(file_contents)} total words")
+    print(get_character_counts(file_contents))
 
 main() 
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def get_num_words(file_contents):
     return len(file_contents.split())
+
+def get_character_counts(file_contents):
+    characters = {}
+    for char in file_contents:
+        char = char.lower()
+        if char in characters:
+            characters[char] += 1
+        else:
+            characters[char] = 1
+    return characters
